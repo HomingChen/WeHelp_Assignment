2bb4523880e90d140737797e0fed93db6314ee58
HomingChen <homing275@gmail.com>
2022-10-19 23:45:06 +0800

    update data.sql with cmd mysqldump

--- week-5/data.sql
+++ week-5/data.sql
@@ -1,69 +1,84 @@
--- 【要求二】
-CREATE DATABASE website;
-USE website;
-CREATE TABLE member (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    follower_count INT UNSIGNED NOT NULL DEFAULT 0,
-    time DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-SHOW COLUMNS FROM member;
-ALTER TABLE member MODIFY COLUMN id BIGINT AUTO_INCREMENT COMMENT '獨立編號';
-ALTER TABLE member MODIFY COLUMN name VARCHAR(255) NOT NULL COMMENT '姓名';
-ALTER TABLE member MODIFY COLUMN username VARCHAR(255) NOT NULL COMMENT '帳戶名稱';
-ALTER TABLE member MODIFY COLUMN password VARCHAR(255) NOT NULL COMMENT '帳戶密碼';
-ALTER TABLE member MODIFY COLUMN follower_count INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '追蹤者數量';
-ALTER TABLE member MODIFY COLUMN time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '註冊時間';
-SHOW FULL COLUMNS FROM member;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
--- 【要求三】
-INSERT INTO member(name, username, password) VALUES('tester', 'test', 'test');
-INSERT INTO member(name, username, password, follower_count) VALUES('安妮亞', 'AnyaDontLike', 'LikePeanut', 999);
-INSERT INTO member(name, username, password, follower_count) VALUES('乙骨優太', 'TrueLove', 'RikaOrimoto', 10);
-INSERT INTO member(name, username, password, follower_count) VALUES('Steven Jobs', 'StayHungry', 'APPLE', 53);
-INSERT INTO member(name, username, password, follower_count) VALUES('Paul Buchheit', 'DontBeEvil', 'Gmail', 1);
-INSERT INTO member(name, username, password, follower_count) VALUES('菜英文', 'PresidentTsai', 'Tjuku', 57);
-SELECT * FROM member;
-SELECT * FROM member ORDER BY time DESC;
-SELECT * FROM member ORDER BY time DESC LIMIT 1,3;
-SELECT * FROM member WHERE username='test';
-SELECT * FROM member WHERE username='test' AND password='test';
-SET sql_safe_updates=0;
-UPDATE member SET name='test2' WHERE username='test';
-SELECT * FROM member WHERE username='test';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 【要求四】
-SELECT COUNT(id), SUM(follower_count), AVG(follower_count) FROM member;
+--
+-- Table structure for table `member`
+--
 
--- 【要求五】
-CREATE TABLE message(
-	id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '獨立編號',
-    member_id BIGINT NOT NULL COMMENT '留言者會員編號',
-    content VARCHAR(255) NOT NULL COMMENT '留言內容',
-    like_count INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '按讚的數量',
-    time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '留言時間',
-    FOREIGN KEY (member_id) REFERENCES member(id)
-);
-SHOW FULL COLUMNS FROM message;
-INSERT INTO message(member_id, content, like_count) VALUES (1, '嗨，大家好~', 6);
-INSERT INTO message(member_id, content, like_count) VALUES (2, '安妮亞跟tester說好~~~', 1);
-INSERT INTO message(member_id, content, like_count) VALUES (3, '呃...嗨', 0);
-INSERT INTO message(member_id, content, like_count) VALUES (4, 'Hello.', 0);
-INSERT INTO message(member_id, content, like_count) VALUES (1, '欸Steven, 聽說iPhone 14 pro系列災情嚴重欸', 6);
-UPDATE message SET like_count=0 WHERE id=5;
-INSERT INTO message(member_id, content, like_count) VALUES (2, '安妮亞也不喜歡', 1);
-INSERT INTO message(member_id, content, like_count) VALUES (4, 'Please contact with apple store service near your location.', 2);
-INSERT INTO message(member_id, content, like_count) VALUES (1, '那我到底要不要跳槽到ios啊...', 3);
-INSERT INTO message(member_id, content, like_count) VALUES (5, '請繼續支持好棒棒的Pixel(ˊ<_ˋ)', 5);
-SELECT * FROM message;
-SELECT message.id, message.member_id, member.name, member.username, message.content, message.like_count, message.time
-	FROM message INNER JOIN member ON message.member_id=member.id;
-SELECT message.id, message.member_id, member.name, member.username, message.content, message.like_count 
-	FROM message INNER JOIN member ON message.member_id=member.id 
-	WHERE member.username='test';
-SELECT message.member_id, member.name, member.username, COUNT(like_count), SUM(like_count), AVG(like_count) 
-	FROM message INNER JOIN member ON message.member_id=member.id 
-	WHERE member.username='test';
-DROP TABLE message;
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `name` varchar(255) NOT NULL COMMENT '姓名',
+  `username` varchar(255) NOT NULL COMMENT '帳戶名稱',
+  `password` varchar(255) NOT NULL COMMENT '帳戶密碼',
+  `follower_count` int unsigned NOT NULL DEFAULT '0' COMMENT '追蹤者數量',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '註冊時間',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'test2','test','test',0,'2022-10-18 22:53:43'),(2,'安妮亞','AnyaDontLike','LikePeanut',999,'2022-10-18 22:56:25'),(3,'乙骨優太','TrueLove','RikaOrimoto',10,'2022-10-18 23:25:58'),(4,'Steven Jobs','StayHungry','APPLE',53,'2022-10-18 23:32:03'),(5,'Paul Buchheit','DontBeEvil','Gmail',1,'2022-10-18 23:32:06'),(6,'菜英文','PresidentTsai','Tjuku',57,'2022-10-18 23:32:07');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `member_id` bigint NOT NULL COMMENT '留言者會員編號',
+  `content` varchar(255) NOT NULL COMMENT '留言內容',
+  `like_count` int unsigned NOT NULL DEFAULT '0' COMMENT '案讚的數量',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '留言時間',
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,1,'嗨，大家好~',6,'2022-10-19 11:00:27'),(2,2,'安妮亞跟tester說好~~~',1,'2022-10-19 13:04:32'),(3,3,'呃...嗨',0,'2022-10-19 13:04:37'),(4,4,'Hello.',0,'2022-10-19 13:04:42'),(5,1,'欸Steven, 聽說iPhone 14 pro系列災情嚴重欸',0,'2022-10-19 13:04:43'),(6,2,'安妮亞也不喜歡',1,'2022-10-19 13:04:46'),(7,4,'Please contact with apple store service near your location.',2,'2022-10-19 13:04:48'),(8,1,'那我到底要不要跳槽到ios啊...',3,'2022-10-19 13:04:50'),(9,5,'請繼續支持好棒棒的Pixel(ˊ<_ˋ)',5,'2022-10-19 13:04:51');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-19 23:42:28
